410bfd338d83bef85a43ae771f21eaf22b5d70df
Alexey Ratnikov <Voblin_ne@mail.ru>
2018-01-31 16:32:52 +0300

    NotSoGoodForms

--- News/admin.py
+++ News/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from News.models import Articles
+from .models import Post
 
+admin.site.register(Post)
 admin.site.register(Articles)
--- News/urls.py
+++ News/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from News.models import Articles
+from . import views
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
                                 template_name="News/posts.html")),
     url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Articles,
-                                             template_name ='News/post.html'))
-
+                                             template_name='News/post.html')),
+    url(r'^post/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit')
 ]
