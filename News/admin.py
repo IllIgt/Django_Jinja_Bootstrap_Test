4a83280af5c7396a614d8d0c36285cd7f2160b71
Alexey Ratnikov <Voblin_ne@mail.ru>
2018-01-31 16:32:56 +0300

    Revert "NotSoGoodForms"
    This reverts commit 410bfd338d83bef85a43ae771f21eaf22b5d70df.

--- News/admin.py
+++ News/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from News.models import Articles
-from .models import Post
 
-admin.site.register(Post)
 admin.site.register(Articles)
--- News/models.py
+++ News/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-
 
 class Articles(models.Model):
     title = models.CharField(max_length=120)
@@ -10,22 +8,3 @@ class Articles(models.Model):
     def __str__(self):
         return self.title
 
-
-class Post(models.Model):
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-    )
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
