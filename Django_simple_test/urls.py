c28cbe46489d4d0c00e8500b97605faaa9e40756
Alexey Ratnikov <Voblin_ne@mail.ru>
2018-01-31 18:09:12 +0300

    GoodFormsNotUseble

--- Django_simple_test/urls.py
+++ Django_simple_test/urls.py
@@ -1,11 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+
 
 urlpatterns = [
     path('adminuser/', admin.site.urls),
     url(r'^', include('mainApp.urls')),
     url(r'^news/', include('News.urls')),
+    url(r'blog/', include('blog.urls')),
 
 ]
